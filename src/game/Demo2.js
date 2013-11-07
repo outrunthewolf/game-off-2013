72c3ed531c0260a4de692790fbd1469321f4c937
Chris <chris@outrunthewolf.com>
2013-11-07 22:22:44 +0000

    Working preloader shizzle

--- src/game/Demo2.js
+++ src/game/Demo2.js
@@ -27,53 +27,48 @@ var Stage = {
 };
 
 
-var Demo2 = new BaseClass({
+/****
+*	
+*	PRELOADER SHIZZLE
+*
+****/
+var holder = false;
+var manifest = false;
+var totalLoaded = 0;
+var stage = false;
+var message = false;
 
-	holder: false,
-	manifest: false,
-	totalLoaded: "test",
-	stage: false,
-	message: false,
-	self: this,
+function init(obj) {
 
-	init: function(obj) {
+	// Set up manifest files
+	manifest = [
+		{src:"assets/images/stars.png", id:"stars"},
+		{src:"assets/images/ship.png", id:"ship"},
+	];
 
-		// Set up manifest files
-		manifest = [
-			{src:"assets/images/stars.png", id:"stars"},
-			{src:"assets/images/ship.png", id:"ship"},
-		];
+	// Create new stage
+	stage = Stage.newStage();
 
-		// Create new stage
-		this.stage = Stage.newStage();
-		console.log(this.stage);
+	// Add a preloader to the stage
+	message = new createjs.Text("CHRIS", "20px; Arial", "#FFF"); 
+	message.x = 20;
+	message.y = 10;
+	stage.addChild(this.message);
+	stage.update();
 
-		// Add a preloader to the stage
-		this.message = new createjs.Text("CHRIS", "20px; Arial", "#FFF"); 
-		this.message.x = 20;
-		this.message.y = 10;
-		this.stage.addChild(this.message);
-		this.stage.update();
+	// Preloader
+	preloader = new createjs.LoadQueue(true);
+	preloader.addEventListener("progress", handleProgress);
+	
+	// Load the manifest for any assets
+	preloader.loadManifest(manifest);
 
-		// Preloader
-		preloader = new createjs.LoadQueue(true);
-		preloader.addEventListener("progress", this.handleProgress);
-		preloader.addEventListener("complete", this.handleComplete);
-		
-		// Load the manifest for any assets
-		preloader.loadManifest(manifest);
+	// Begin the preloader
+	preloader.load();
+}
 
-		// Begin the preloader
-		preloader.load();
-	},
-
-	// Handle a file load
-	handleProgress: function() {
-		console.log(this.message);
-		self.message.text = "Loading " + (preloader.progress * 100 | 0) + "%";
-		stage.update();
- 	}
-
-
-});
-
+// Handle a file load
+function handleProgress() {
+	message.text = "Loading " + (preloader.progress * 100 | 0) + "%";
+	stage.update();
+}
